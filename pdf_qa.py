8f747e72dc88a8d4887da8452e5e85e3837b28aa
Johannes Hackbarth <j.hackbarth@cubegaming.de>
2024-01-06 15:57:57 +0100

    Add persisting chromadb

--- pdf_qa.py
+++ pdf_qa.py
@@ -12,14 +12,16 @@ from langchain.vectorstores import Chroma
 
 load_dotenv()
 
-
-loader = PyPDFLoader(os.environ["DOCUMENT_PATH"])
-documents = loader.load_and_split()
-
 embeddings = OpenAIEmbeddings()
-docsearch = Chroma.from_documents(documents, embeddings, persist_directory="./chroma.db")
 
-qa = RetrievalQA.from_chain_type(llm=ChatOpenAI(model_name="gpt-4-1106-preview", temperature=0.0), 
-                                 chain_type="stuff", 
+if not os.path.exists("./chromadb"):
+    loader = PyPDFLoader(os.environ["DOCUMENT_PATH"])
+    documents = loader.load_and_split()
+    docsearch = Chroma.from_documents(documents, embeddings, persist_directory="./chromadb")
+else:
+    vectordb = Chroma(persist_directory="./chromadb", embedding_function=embeddings)
+
+qa = RetrievalQA.from_chain_type(llm=ChatOpenAI(model_name="gpt-4-1106-preview", temperature=0.0),
+                                 chain_type="stuff",
                                  retriever=docsearch.as_retriever(search_type="mmr", search_kwargs={'k': 5, 'fetch_k': 50}),
                                  return_source_documents=True)
