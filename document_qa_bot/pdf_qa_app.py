bef7fcf88e6b7ea4725b7a58254fa8eb5498dd96
Johannes Hackbarth <j.hackbarth@cubegaming.de>
2024-01-14 17:56:54 +0100

    Fix response rendering and add source references

--- document_qa_bot/pdf_qa_app.py
+++ document_qa_bot/pdf_qa_app.py
@@ -11,15 +11,15 @@ from langchain.chains import RetrievalQA
 from langchain.document_loaders import PyPDFLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
 from langchain.vectorstores import Chroma
 
 
 load_dotenv()
 
 app = Flask(__name__)
-
-# Your existing code here
 embeddings = OpenAIEmbeddings()
+model = os.environ["MODEL_NAME"]
 
 if not os.path.exists("./chromadb"):
     loader = PyPDFLoader(os.environ["DOCUMENT_PATH"])
@@ -28,7 +28,7 @@ if not os.path.exists("./chromadb"):
 else:
     docsearch = Chroma(persist_directory="./chromadb", embedding_function=embeddings)
 
-qa = RetrievalQA.from_chain_type(llm=ChatOpenAI(model_name="gpt-4-1106-preview", temperature=0.0),
+qa = RetrievalQA.from_chain_type(llm=ChatOpenAI(model_name=model, temperature=0.0, max_tokens=2048),
                                     chain_type="stuff",
                                     retriever=docsearch.as_retriever(search_type="mmr", search_kwargs={'k': 5, 'fetch_k': 50}),
                                     return_source_documents=True)
@@ -38,15 +38,16 @@ qa = RetrievalQA.from_chain_type(llm=ChatOpenAI(model_name="gpt-4-1106-preview",
 def qa_endpoint():
     if request.method == 'POST':
         # Get the input question from the request
-        question = request.form['question']
+        question = request.form['query']
+        prompt_template = PromptTemplate.from_template(os.environ["PROMPT_TEMPLATE"])
+        query = prompt_template.format(query=question)
 
         # Use the QA model to get the answer
-        answer = qa(question)
-
-    	# answer['source_documents']
-
+        answer = qa(query)
+        source_documents = [doc.metadata['source'] + ' p.'  + str(doc.metadata['page']) + '<br>'  for doc in answer['source_documents']]
         # Return the answer as a JSON response
-        return jsonify({'answer': answer['result']})
+        return jsonify({'answer': answer['result'], 
+                        'source_documents': source_documents})
     else:
         # Render the HTML template with the input field and output
         return render_template('index.html')
