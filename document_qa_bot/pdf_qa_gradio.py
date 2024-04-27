2800dbfab0679696d914c9aedf61485c8c43f6be
Johannes Hackbarth <j.hackbarth@cubegaming.de>
2024-04-27 18:19:48 +0200

    Refactor document_qa_gradio.py and add DocumentStore class

--- document_qa_bot/pdf_qa_gradio.py
+++ document_qa_bot/pdf_qa_gradio.py
@@ -1,48 +1,42 @@
 import os
-import glob
-
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 from dotenv import load_dotenv
 
 from langchain.chains import RetrievalQA
-from langchain.document_loaders import PyPDFDirectoryLoader
 from langchain.globals import set_llm_cache
-from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.cache import InMemoryCache
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
-from langchain.vectorstores import Chroma
+
+from tools.document_store import DocumentStore
 
 import gradio as gr
 
 load_dotenv()
 
-embeddings = OpenAIEmbeddings()
 model = os.environ["MODEL_NAME"]
 path = os.environ["DOCUMENT_PATH"]
+chromadb_path = os.environ["CHROMADB_PATH"]
 
-if not os.path.exists("./chromadb"):
-    loader = PyPDFDirectoryLoader(path, glob="*5D*.pdf")
-    documents = loader.load_and_split()
-
-    docsearch = Chroma.from_documents(documents, embeddings, persist_directory="./chromadb")
+if not os.path.exists(chromadb_path):
+    docsearch = DocumentStore(path=chromadb_path)
+    docsearch.generate_embeddings()
+    docsearch.fill_chroma()
 else:
-    docsearch = Chroma(persist_directory="./chromadb", embedding_function=embeddings)
+    docsearch = DocumentStore(path=chromadb_path)
 
-qa = RetrievalQA.from_chain_type(llm=ChatOpenAI(model_name=model, temperature=0.0, max_tokens=2048),
-                                    chain_type="stuff",
-                                    retriever=docsearch.as_retriever(search_type="mmr", search_kwargs={'k': 5, 'fetch_k': 50}),
-                                    return_source_documents=True)
+llm = ChatOpenAI(model_name=model, temperature=0.0, max_tokens=2048)
+retriever = docsearch.get_chroma().as_retriever(search_type="mmr", search_kwargs={'k': 5, 'fetch_k': 50})
+
+rag_chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
+
 set_llm_cache(InMemoryCache())
 
 def qa_endpoint(message, history):
     if message is not None:
         prompt_template = PromptTemplate.from_template(os.environ["PROMPT_TEMPLATE"])
         query = prompt_template.format(query=message)
-        response = qa(query)
+        response = rag_chain(query)
         source_documents = [doc.metadata['source'] + ' p.'  + str(doc.metadata['page']) for doc in response['source_documents']]
 
         return response["result"].join(source_documents)
@@ -52,11 +46,11 @@ def qa_endpoint_streaming(message, history):
         prompt_template = PromptTemplate.from_template(os.environ["PROMPT_TEMPLATE"])
         query = prompt_template.format(query=message)
         partial_message = ""
-        for chunk in qa.stream({'query': query}):
+        for chunk in rag_chain.stream({'query': query}):
             partial_message = partial_message + chunk['result']
             yield partial_message
             if 'source_documents' in chunk:
                 source_documents = [doc.metadata['source'] + ' p.'  + str(doc.metadata['page']) + '\n'  for doc in chunk['source_documents']]
                 yield partial_message + "\n" + "\n".join(source_documents)
 
-gr.ChatInterface(qa_endpoint_streaming).queue().launch(share=True, debug=True)
+gr.ChatInterface(qa_endpoint).queue().launch(share=False, debug=True)
---
+++ document_qa_bot/tools/document_store.py
@@ -0,0 +1,38 @@
+import os
+
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
+from langchain.document_loaders import PyPDFDirectoryLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+
+class DocumentStore:
+
+    def __init__(self, path: str = None) -> None:
+        self.embeddings = OpenAIEmbeddings()
+        self.documents = None
+        self.docsearch = None
+        self.path: str = path if path is not None else "./chromadb"
+
+    def generate_embeddings(self) -> None:
+        path = os.environ["DOCUMENT_PATH"]
+        loader = PyPDFDirectoryLoader(path, glob="*5D*.pdf")
+        self.documents = loader.load_and_split()
+
+    def get_embeddings(self) -> OpenAIEmbeddings:
+        return self.embeddings
+
+    def fill_chroma(self) -> None:
+        if not os.path.exists(self.path):
+            self.docsearch = Chroma.from_documents(self.documents, self.embeddings, persist_directory=self.path)
+        else:
+            self.docsearch = Chroma(persist_directory=self.path, embedding_function=self.embeddings)
+
+    def get_chroma(self) -> Chroma:
+        if self.docsearch is None:
+            self.docsearch = Chroma(persist_directory=self.path, embedding_function=self.embeddings)
+        return self.docsearch
+    
